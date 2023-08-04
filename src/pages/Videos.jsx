d1311a0c5c2d029256722b26562180e5e53422a2
chaud1024 <chaud1024@gmail.com>
2023-08-04 11:55:37 +0900

    #12-12 context사용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,23 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
-import FakeYoutube from "../api/fakeYoutube";
 import VideoCard from "../components/Video/VideoCard";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Video() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () =>
-    // search(keyword) 아래의 클래스 안 쓰고 바로 youtube.js 사용시
-    // 비동기 내부 네트워크 구현사항을 별도의 네트워크 담당 모듈로 설정
-    {
-      const youtube = new FakeYoutube();
-      return youtube.search(keyword);
-    },
-  );
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
+
   return (
     <div>
       {keyword ? `search: ${keyword}` : "🔥"}
