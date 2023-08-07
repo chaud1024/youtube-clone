6f83e18b19b9d47c7ad0217126c4954637562e8d
chaud1024 <chaud1024@gmail.com>
2023-08-07 14:42:06 +0900

    #12-20 상세화면 iframe과 video.id를 통해 영상 보여주기

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,51 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import ChannelInfo from "../components/Channel/ChannelInfo";
+import RelatedVideos from "../components/Video/RelatedVideos";
 
 export default function VideoDetail() {
-  const { videoId } = useParams();
-  const navigate = useNavigate();
+  const {
+    state: { video },
+  } = useLocation();
 
-  const [data, setData] = useState({});
+  console.log("video", video);
 
-  useEffect(() => {
-    fetch(`/data/related-videos.json`)
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  const { title, channelId, channelTitle, description } = video.snippet;
 
-  const related = data.items;
   return (
-    <div>
-      VideoDetail - videoId : {videoId}
-      <div className="flex gap-4 flex-col lg:flex-row">
+    <section className="w-full">
+      <article className="w-full">
         <div className="w-full lg:w-3/5 aspect-[16/9]">
           <iframe
             width="100%"
             height="100%"
-            src={`https://www.youtube.com/embed/${videoId}`}
+            src={`https://www.youtube.com/embed/${video.id}`}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
         </div>
-        <ul className="w-2/5">
-          {related.map((item, index) => (
-            <li
-              key={index}
-              className="flex"
-              onClick={() => navigate(`/videos/${item.id.videoId}`)}>
-              <div className="overflow-hidden w-96 h-24 flex justify-center items-center">
-                <img
-                  src={item.snippet.thumbnails.standard.url}
-                  alt={`thumbnail of ${item.snippet.title}`}
-                  className="w-full h-auto"
-                />
-              </div>
-              <p>{item.snippet.title}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+        <h2>{title}</h2>
+        <ChannelInfo id={channelId} name={channelTitle} />
+        <pre>{description}</pre>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
   );
 }
